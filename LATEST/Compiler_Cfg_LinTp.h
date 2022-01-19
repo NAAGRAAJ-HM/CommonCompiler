a7ced4498204bb4db70cd9504d79323322ffa666
root <nagaraja.hm.0001@gmail.com>
2022-01-19 12:16:48 +0100

    Update: Compiler abstraction for GNU compiler toolchain

---
+++ LATEST/Compiler_Cfg_LinTp.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#define LINTP_CODE                      /*TBD*/
+#define LINTP_CODE_CB                   /*TBD*/
+#define LINTP_CODE_FAST                 /*TBD*/
+#define LINTP_CODE_SLOW                 /*TBD*/
+#define LINTP_CONST                     /*TBD*/
+#define LINTP_CALIB                     /*TBD*/
+#define LINTP_CONFIG_DATA               /*TBD*/
+#define LINTP_CONST_SAVED_RECOVERY_ZONE /*TBD*/
+#define LINTP_APPL_DATA                 /*TBD*/
+#define LINTP_APPL_CONST                /*TBD*/
+#define LINTP_VAR                       /*TBD*/
+#define LINTP_VAR_FAST                  /*TBD*/
+#define LINTP_VAR_SLOW                  /*TBD*/
+#define LINTP_INTERNAL_VAR              /*TBD*/
+#define LINTP_VAR_SAVED_ZONE            /*TBD*/
+
