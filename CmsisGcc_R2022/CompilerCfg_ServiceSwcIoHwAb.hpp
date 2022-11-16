c378712affa31dc03e93f42d071cf3d5751ce2d0
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2022-11-16 12:00:28 +0100

    Update: Compiler abstraction for GNU tool-chain

--- CmsisGcc_R2022/CompilerCfg_ServiceSwcIoHwAb.hpp
+++ CmsisGcc_R2022/CompilerCfg_ServiceSwcIoHwAb.hpp
@@ -1,6 +1,6 @@
 #pragma once
 /******************************************************************************/
-/* File   : CompilerCfg_ServiceEcuM.hpp                                       */
+/* File   : CompilerCfg_ServiceSwcIoHwAb.hpp                                  */
 /* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
 /******************************************************************************/
 
@@ -11,21 +11,21 @@
 /******************************************************************************/
 /* #DEFINES                                                                   */
 /******************************************************************************/
-#define SERVICEECUM_CODE                                                 /*TBD*/
-#define SERVICEECUM_CODE_CB                                              /*TBD*/
-#define SERVICEECUM_CODE_FAST                                            /*TBD*/
-#define SERVICEECUM_CODE_SLOW                                            /*TBD*/
-#define SERVICEECUM_CONST                                                /*TBD*/
-#define SERVICEECUM_CALIB                                                /*TBD*/
-#define SERVICEECUM_CONFIG_DATA                                          /*TBD*/
-#define SERVICEECUM_CONST_SAVED_RECOVERY_ZONE                            /*TBD*/
-#define SERVICEECUM_APPL_DATA                                            /*TBD*/
-#define SERVICEECUM_APPL_CONST                                           /*TBD*/
-#define SERVICEECUM_VAR                                                  /*TBD*/
-#define SERVICEECUM_VAR_FAST                                             /*TBD*/
-#define SERVICEECUM_VAR_SLOW                                             /*TBD*/
-#define SERVICEECUM_INTERNAL_VAR                                         /*TBD*/
-#define SERVICEECUM_VAR_SAVED_ZONE                                       /*TBD*/
+#define SERVICESWCIOHWAB_CODE                                            /*TBD*/
+#define SERVICESWCIOHWAB_CODE_CB                                         /*TBD*/
+#define SERVICESWCIOHWAB_CODE_FAST                                       /*TBD*/
+#define SERVICESWCIOHWAB_CODE_SLOW                                       /*TBD*/
+#define SERVICESWCIOHWAB_CONST                                           /*TBD*/
+#define SERVICESWCIOHWAB_CALIB                                           /*TBD*/
+#define SERVICESWCIOHWAB_CONFIG_DATA                                     /*TBD*/
+#define SERVICESWCIOHWAB_CONST_SAVED_RECOVERY_ZONE                       /*TBD*/
+#define SERVICESWCIOHWAB_APPL_DATA                                       /*TBD*/
+#define SERVICESWCIOHWAB_APPL_CONST                                      /*TBD*/
+#define SERVICESWCIOHWAB_VAR                                             /*TBD*/
+#define SERVICESWCIOHWAB_VAR_FAST                                        /*TBD*/
+#define SERVICESWCIOHWAB_VAR_SLOW                                        /*TBD*/
+#define SERVICESWCIOHWAB_INTERNAL_VAR                                    /*TBD*/
+#define SERVICESWCIOHWAB_VAR_SAVED_ZONE                                  /*TBD*/
 
 /******************************************************************************/
 /* MACROS                                                                     */
