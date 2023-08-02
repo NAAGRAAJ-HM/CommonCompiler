32caa51de0a51d0458e9b9280ac7423f43d94de1
Raajnaag Huliyapurada Mata <nagaraja.hm.0001@gmail.com>
2023-08-02 08:46:56 +0200

    Initial commit

--- GhsMulti_v2015p01p08/CompilerCfg_SwcServiceEcuM.h
+++ GhsMulti_v2015p01p08/CompilerCfg_SwcServiceEcuM.h
@@ -44,6 +44,23 @@
 #define SWCSERVICEECUM_INTERNAL_VAR                                      /*TBD*/
 #define SWCSERVICEECUM_VAR_SAVED_ZONE                                    /*TBD*/
 
+//TBD: Standardize for all modules/components (+ Cfg and Appl)
+#define SWCAPPLECUM_CODE                                                 /*TBD*/
+#define SWCAPPLECUM_CODE_CB                                              /*TBD*/
+#define SWCAPPLECUM_CODE_FAST                                            /*TBD*/
+#define SWCAPPLECUM_CODE_SLOW                                            /*TBD*/
+#define SWCAPPLECUM_CONST                                                /*TBD*/
+#define SWCAPPLECUM_CALIB                                                /*TBD*/
+#define SWCAPPLECUM_CONFIG_DATA                                          /*TBD*/
+#define SWCAPPLECUM_CONST_SAVED_RECOVERY_ZONE                            /*TBD*/
+#define SWCAPPLECUM_APPL_DATA                                            /*TBD*/
+#define SWCAPPLECUM_APPL_CONST                                           /*TBD*/
+#define SWCAPPLECUM_VAR                                                  /*TBD*/
+#define SWCAPPLECUM_VAR_FAST                                             /*TBD*/
+#define SWCAPPLECUM_VAR_SLOW                                             /*TBD*/
+#define SWCAPPLECUM_INTERNAL_VAR                                         /*TBD*/
+#define SWCAPPLECUM_VAR_SAVED_ZONE                                       /*TBD*/
+
 /******************************************************************************/
 /* MACROS                                                                     */
 /******************************************************************************/
